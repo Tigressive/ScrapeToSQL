64458fe8d5b5bd7e0d3ac8c7690ccc213963015c
Ashton Atkinson <rascalcrafters@gmail.com>
2022-07-20 10:41:42 -0600

    Iterates through all files in directory.

--- main.py
+++ main.py
@@ -39,32 +39,32 @@ def connect_sql(bname, bkeyword, baddress, bcity, bstate, bzip, bwebsite, bphone
 
 def read_csv():
     entries = os.listdir('test/')
-    if len(entries) > 0:
-        with open(f'test/{entries[0]}') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            next(csv_reader)
+    for files in entries:
+        if len(entries) > 0:
+            with open(f'test/{files}') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                next(csv_reader)
 
-            for row in csv_reader:
-                if not row[11]:
-                    print("No Email")
-                else:
-                    bname = row[2]
-                    bkeyword = row[1]
-                    baddress = row[3]
-                    bcity = row[5]
-                    bstate = row[6]
-                    bzip = row[7]
-                    bwebsite = row[9]
-                    bphone = row[10]
-                    bemail = row[11]
+                for row in csv_reader:
+                    if not row[11]:
+                        print("No Email")
+                    else:
+                        bname = row[2]
+                        bkeyword = row[1]
+                        baddress = row[3]
+                        bcity = row[5]
+                        bstate = row[6]
+                        bzip = row[7]
+                        bwebsite = row[9]
+                        bphone = row[10]
+                        bemail = row[11]
 
-                    connect_sql(bname, bkeyword, baddress, bcity, bstate, bzip, bwebsite, bphone, bemail)
+                        connect_sql(bname, bkeyword, baddress, bcity, bstate, bzip, bwebsite, bphone, bemail)
 
-            print(f"Successfully Imported {entries[0]} ")
-            os.replace(f'test/{entries[0]}', f'done/{entries[0].replace(".csv", "_done.csv" )}')
-
-    else:
-        print("Empty - Time to start a new scrape!")
+            print(f"Successfully Imported {files} ")
+            os.replace(f'test/{files}', f'done/{files.replace(".csv", "_done.csv" )}')
+        else:
+            print("Empty - Time to start a new scrape!")
 
 
 if __name__ == '__main__':
