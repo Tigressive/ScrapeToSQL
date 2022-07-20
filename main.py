f511abb4455a9d2326be172476ed82d45b559bab
Ashton Atkinson <rascalcrafters@gmail.com>
2022-07-20 16:46:24 -0600

    Clean up code.

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-# This is a sample Python script.
-
 import csv
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import os
 import time
-
 import pymysql
 import schedule as schedule
 
